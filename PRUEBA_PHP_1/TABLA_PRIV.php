36b7b7b7e029e0d5d9803122684dd59431d13b3a
truiz5 <aruiz@florida-uni.es>
2015-05-11 17:10:26 +0200

    Primera prueba de GIT desde eclips_PHP
    Signed-off-by: truiz5 <aruiz@florida-uni.es>

---
+++ PRUEBA_PHP_1/TABLA_PRIV.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+class Tabla {
+  private $mat=array();
+  private $cantFilas;
+  private $cantColumnas;
+  public function __construct($fi,$co)
+  {
+    $this->cantFilas=$fi;
+    $this->cantColumnas=$co;
+  }
+  
+  public function cargar($fila,$columna,$valor)
+  {
+    $this->mat[$fila][$columna]=$valor;
+  }
+
+  private function inicioTabla()
+  {
+    echo '<table border="1">';
+  }
+
+  private function inicioFila()
+  {
+    echo '<tr>';
+  }
+  
+  private function mostrar($fi,$co)
+  {
+    echo '<td>'.$this->mat[$fi][$co].'</td>';
+  }
+
+  private function finFila()
+  {
+    echo '</tr>';
+  }
+
+  private function finTabla()
+  {
+    echo '</table>';
+  }
+
+  public function graficar()
+  {
+    $this->inicioTabla();
+    for($f=1;$f<=$this->cantFilas;$f++)
+    {
+      $this->inicioFila();
+      for($c=1;$c<=$this->cantColumnas;$c++)
+      {
+        $this->mostrar($f,$c);
+      } 
+      $this->finFila();
+    }
+    $this->finTabla();
+  }
+}
+
+$tabla1=new Tabla(2,3);
+$tabla1->cargar(1,1,"1");
+$tabla1->cargar(1,2,"2");
+$tabla1->cargar(1,3,"3");
+$tabla1->cargar(2,1,"4");
+$tabla1->cargar(2,2,"5");
+$tabla1->cargar(2,3,"6");
+$tabla1->graficar();
+?>
+
+
+
+
+
+
+
+
+</body>
+</html>
